b44fa5bc99b9d0f0cf26e46b15b67d8dd96c0e63
vhogberg <119532259+vhogberg@users.noreply.github.com>
2025-07-09 12:12:21 +0200

    Continued styling on timeline item

--- src/pages/MainContent/Experience/Experience.tsx
+++ src/pages/MainContent/Experience/Experience.tsx
@@ -7,9 +7,6 @@ function Experience () {
     return (
         <>
         <h1>Erfarenhet</h1>
-        <p>ERR. 404: Tidigare jobberfarenhet inom programmering ej funnet..</p>
-        <p>Övriga tidigare erfarenheter:</p>
-
         <Timeline items={experienceItems}/>
         </>
     );
